b745c8ac8cbc317cfae7b43d20f9b1c363377472
Lorenzo Magro <magrolorenzo@gmail.com>
2020-11-27 16:04:55 +0100

    Non stampa nomi attori anche se recuperati

--- assets/main.js
+++ assets/main.js
@@ -1,39 +1,4 @@
-// MILESTONE 1
-// Creare un layout di base con una barra di ricerca composta da un input e un pulsante.
-// Quando l'utente clicca sul pulsante, facciamo una chiamata all'API
-// https://api.themoviedb.org/3/search/movie
-//ricordandoci di passare la nostra API key e la query di ricerca,
-// ossia il testo inserito dall'utente nell'input.
-
-// api_key: 9288442951ff78eee0e3f39d2a7b597e
-
-// Con i risultati che riceviamo, visualizziamo in pagina una card per ogni film,
-// stampando per ciascuno:
-// titolo
-// titolo in lingua originale
-// lingua originale
-// voto
-
-// MILESTONE 2
-// La seconda milestone è a sua volta suddivisa in 3 punti:
-
-// 1- sostituire il voto numerico su base 10 in un voto su base 5
-// e visualizzare in totale 5 stelline, di cui tante piene quanto è il voto arrotondato
-// (non gestiamo stelline a metà).
-// Ad esempio, se il voto è 8.2, dobbiamo visualizzare 4 stelline piene
-// e 1 stellina vuota (in totale sempre 5)
-
-// 2- sostituire la lingua con una bandierina che identifica il paese.
-// Suggerimento: scarichiamo una manciata di bandierine relative alle lingue che vogliamo gestire
-// (attenzione che la lingua è "en", non "us" o "uk" :wink: ).
-// Quindi andremo ad inserire solamente le bandierine che sappiamo di avere,
-// mentre per le altre lingue di cui non abbiamo previsto la bandierina,
-// lasciamo il codice della lingua testuale
-
-
-// 3- aggiungere ai risultati anche le serie tv.
-// Attenzione che alcune chiavi per le serie tv sono diverse da quelle dei film,
-// come ad esempio "title" per i film e "name" per le serie
+// Boolflix
 
 var app = new Vue({
     el:"#root",
@@ -42,10 +7,6 @@ var app = new Vue({
         search: "",
         text_searched: "",
 
-        // Array dei risultati movie
-        movies_array: [],
-        // Array dei risultati dei TV show
-        tv_array: [],
         // Array dei risultati totali
         total_array: [],
         // Array di tutti i generi di Movies e TV shows
@@ -75,10 +36,6 @@ var app = new Vue({
                 this.isSearching = true;
                 this.text_searched = this.search;
 
-                // Array dei risultati movie
-                this.movies_array = [],
-                // Array dei risultati dei TV show
-                this.tv_array = [],
                 // Array dei risultati totali
                 this.total_array = [],
                 // id:movie + array text generi + array 5 attori
@@ -91,9 +48,9 @@ var app = new Vue({
                         query: this.text_searched
                     }}
                 ).then((searched_movie) =>{
-                    this.movies_array = (searched_movie.data.results);
+                    let movies_array = (searched_movie.data.results);
                     // Aggiungo all array finale i risultati dei film
-                    this.total_array = this.total_array.concat(this.movies_array);
+                    this.total_array = this.total_array.concat(movies_array);
                 });
 
                 // Get per recuperare la lista dei tv show
@@ -103,23 +60,22 @@ var app = new Vue({
                         query: this.text_searched
                     }}
                 ).then((searched_tv_show) =>{
-                    this.tv_array = (searched_tv_show.data.results);
+                    let tv_array = (searched_tv_show.data.results);
 
                     // Concateno i due array dei risultati dei film e tv shows nell array finale
-                    this.total_array = this.total_array.concat(this.tv_array);
+                    this.total_array = this.total_array.concat(tv_array);
                     this.isSearching = false;
                     this.search = "";
 
                     // Mi creo all'interno di ogni oggetto film due stringhe per visualizzare generi e 5 attori
                     this.total_array.forEach((movie) => {
-
                         this.additional_infos.push(this.get_add_infos(movie));
-                        // movie.genres_string = this.get_genres(movie.genre_ids);
-                        // movie.mainActors = this.get_credits(movie.id);
                     });
-                    console.log(this.additional_infos);
 
-                    // console.log(this.total_array[0]);
+                    console.log("Lunghezza additional infos: " + this.additional_infos.length);
+                    console.log(this.additional_infos);
+                    console.log("Lunghezza total array: " + this.total_array.length);
+                    console.log(this.total_array);
 
                 });
             }
@@ -141,39 +97,38 @@ var app = new Vue({
             this.poster_is_hide =  false;
         },
 
+        // Funzione per creazione oggetto di info addizionali nel array additional info
         get_add_infos(movie){
 
-            let add_info_obj ={
-                id: movie.id,
-                genres: this.get_genres(movie.genre_ids),
-                main_actors:[]
-            }
+            let new_id = movie.id;
+            let new_genres = this.get_genres(movie.genre_ids);
+            let new_actors = this.get_credits(movie.id);
 
+            let add_info_obj ={
+                id: new_id,
+                genres: new_genres,
+                main_actors: new_actors
+            };
             return add_info_obj;
         },
 
+        // Funzione per recupero dei generi del film in formato stringa
         get_genres(genre_ids){
-            //  genre_id = [ 100 , 45 ]
+            // Esempio di genre_ids = [ 100 , 45 ]
             let genres = [];
 
                 genre_ids.forEach(genre_id => {
                     this.all_genres.forEach((genre, index) => {
                     // Confronta ogni id del film con l array del genere
                     if(genre_id == genre.id){
-                        console.log("found");
                         // Quando trova corrispondenza aggiunge il nome del genre alla stringa
                         genres.push(genre.name);
-                        // if (genres_string.length == 0) {
-                        //     genres_string = genres_string + genre.name;
-                        // }else{
-                        //     genres_string = genres_string + ", " + genre.name;
-                        // };
                     };
                 });
             });
             return genres;
         },
-
+        // Funzione per la stampa a video dell array dei generi
         print_genres_string(id){
             let genres_string = "";
 
@@ -185,23 +140,35 @@ var app = new Vue({
             return genres_string;
         },
 
+        // Funzione per recupero dei primi 5 nomi di attori del film
         get_credits(id){
 
-            let credits_string = "";
-
-            axios.get(("https://api.themoviedb.org/3/movie/" + id + "/credits") , {
+            axios.get( ("https://api.themoviedb.org/3/movie/" + id + "/credits") , {
                 params: {
                     api_key: this.api_key
                 }}
             ).then((credits) =>{
+                let five_actors = "";
                 for (var i = 0; i < 5; i++) {
-                    // console.log(credits.data.cast[i].name);
-                    credits_string = credits_string + ", " + credits.data.cast[i].name;
+                    let name = credits.data.cast[i].name;
+                    console.log(name);
+                    five_actors = five_actors + ", " + name;
                 };
-                // console.log("Stringa attori " + credits_string);
-                return credits_string;
+                return five_actors;
             });
-        }
+        },
+
+        // Stampa a video della stringa di attori
+        print_actors_string(id){
+            let actors_string = "";
+
+            for (var i = 0; i < this.additional_infos.length; i++) {
+                if(id == this.additional_infos[i].id){
+                    actors_string = this.additional_infos[i].main_actors;
+                };
+            };
+            return actors_string;
+        },
 
     }, // Chiusura Methods
 
